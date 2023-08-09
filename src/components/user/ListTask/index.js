6f67de26fa811120358f10a693c29e90511fd3e8
ongimkshp <doducmanh.bb@gmail.com>
2023-08-10 00:28:54 +0700

    Edit navbar, list item

---
+++ src/components/user/ListTask/index.js
@@ -0,0 +1,37 @@
+import { Checkbox, List, Space, Tag } from 'antd';
+import React from 'react';
+
+const ListTask = ({ list, showDrawer, type }) => {
+  return (
+    <List
+      className="px-8"
+      itemLayout="horizontal"
+      dataSource={list}
+      renderItem={(item) => (
+        <List.Item className="cursor-pointer">
+          {type !== 'todo' && (
+            <Checkbox className="mr-4" checked={type === 'done'} />
+          )}
+          <List.Item.Meta
+            title={
+              <div className="flex">
+                <h1 className="mr-12 text-xl">{item.name}</h1>
+                <Space size={[0, 8]} wrap>
+                  <Tag color="#2db7f5">Private</Tag>
+                  <Tag color="#87d068">Homework</Tag>
+                  <Tag color="#108ee9">Relax</Tag>
+                </Space>
+              </div>
+            }
+            onClick={showDrawer}
+            description={item.members?.join(', ')}
+          />
+
+          <div className="text-gray-400">{item.endDate}</div>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default ListTask;
--- src/components/user/home/index.js
+++ src/components/user/home/index.js
@@ -1,44 +1,17 @@
-import {
-  AppstoreOutlined,
-  MailOutlined,
-  SettingOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-import {
-  Avatar,
-  Button,
-  Checkbox,
-  DatePicker,
-  Drawer,
-  Form,
-  Input,
-  List,
-  Menu,
-} from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, DatePicker, Drawer, Form, Input } from 'antd';
 import { useContext, useEffect, useState } from 'react';
 import axiosApi from '../../../api/axiosApi';
+import { home } from '../../../constants/pages/home';
 import { AuthContext } from '../../../providers/AuthProvider';
+import ListTask from '../ListTask';
+import Sidebar from '../Sidebar';
+
 const API_URL = '/auth/logout';
 
-function getItem(label, key, icon, children, type) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  };
-}
-const items = [
-  getItem('Tasks', 'tasks', <MailOutlined />),
-  getItem('SharingTasks', 'sharing-tasks', <AppstoreOutlined />),
-  getItem('History', 'history', <SettingOutlined />),
-  getItem('Notifications', 'notifications', <SettingOutlined />),
-];
-const count = 3;
-const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`;
 const Home = () => {
   const { authenticated, logout } = useContext(AuthContext);
+
   const handleLogout = async () => {
     try {
       await axiosApi.post(API_URL);
@@ -58,18 +31,86 @@ const Home = () => {
   const onClose = () => {
     setOpen(false);
   };
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
   const [list, setList] = useState([]);
+  const [selectedKey, setSelectedKey] = useState('todo');
+
+  const data = {
+    todo: [
+      {
+        id: 1,
+        name: 'Drink milk',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+      },
+      {
+        id: 2,
+        name: 'Eat eggs',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+      },
+      {
+        id: 3,
+        name: 'Eat bread',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+      },
+    ],
+    inProgress: [
+      {
+        id: 1,
+        name: 'Drink milk',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+      },
+      {
+        id: 2,
+        name: 'Eat eggs',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+      },
+      {
+        id: 3,
+        name: 'Eat bread',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+      },
+    ],
+    done: [
+      {
+        id: 1,
+        name: 'Done task 1',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+      },
+      {
+        id: 2,
+        name: 'Done task 2',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+      },
+    ],
+    sharingTasks: [
+      {
+        id: 1,
+        name: 'Sharing task 1',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+        members: ['member 1', 'member 2', 'member 3'],
+      },
+      {
+        id: 2,
+        name: 'Sharing task 2',
+        endDate: '2021-06-28 12:00',
+        startDate: '2021-06-28 12:00',
+        members: ['member 1', 'member 2', 'member 3'],
+      },
+    ],
+    history: ['History 1', 'History 2', 'History 3'],
+  };
+
   useEffect(() => {
-    fetch(fakeDataUrl)
-      .then((res) => res.json())
-      .then((res) => {
-        setInitLoading(false);
-        setData(res.results);
-        setList(res.results);
-      });
-  }, []);
+    setList(data[selectedKey]);
+  }, [selectedKey]);
 
   return (
     <div className="flex">
@@ -78,28 +119,16 @@ const Home = () => {
           <Avatar size={64} icon={<UserOutlined />} className="mr-4" />
           <h2>Username</h2>
         </div>
-        <div className="border-b-2 border-gray-200 "></div>
-        <Menu mode="inline" items={items} className="pt-8 h-4/5" />;
+        <div className="border-b-2 border-gray-200 " />
+        <Sidebar setSelectedKey={setSelectedKey} />
         <div>
-          <Button className="w-full">Logout</Button>
+          <Button className="w-full">{home.logout}</Button>
         </div>
       </div>
 
       <div className="flex-1 h-screen overflow-y-auto relative">
-        <h1 className="my-8 text-3xl px-4">Tasks</h1>
-        <List
-          className="px-8"
-          loading={initLoading}
-          itemLayout="horizontal"
-          dataSource={list}
-          renderItem={(item) => (
-            <List.Item className="cursor-pointer">
-              <Checkbox className="mr-4" />
-              <List.Item.Meta title={item.name?.last} onClick={showDrawer} />
-              <div className="text-gray-400">2023/06/28 12:00</div>
-            </List.Item>
-          )}
-        />
+        <h1 className="my-8 text-3xl px-4">{home.tasks}</h1>
+        <ListTask list={list} showDrawer={showDrawer} type={selectedKey} />
         <Form className="absolute bottom-0 w-full px-8">
           <Form.Item>
             <Input
@@ -108,8 +137,9 @@ const Home = () => {
               className="h-12"
               status="warning"
             />
+            <Button type="primary">{/* <SendOutlined /> */}Send</Button>
           </Form.Item>
-          <Form.Item className="absolute right-8 top-2 border-l-2">
+          <Form.Item className="border-l-2 absolute right-20 top-2">
             <DatePicker className="w-full" bordered={false} />
           </Form.Item>
         </Form>
